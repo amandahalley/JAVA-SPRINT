e98b04ffa03e2cfcf8d89821deb04de435254bd2
ahllyy <amanda.halley@keyin.com>
2024-10-23 18:53:08 -0230

    create constructor

--- LibraryItem.java
+++ LibraryItem.java
@@ -1,3 +1,4 @@
+//Creating class for library items
 public class LibraryItem {
  private String id;
  private String title;
@@ -5,6 +6,16 @@ public class LibraryItem {
  private String isbn;
  private String publisher;
  private int copies;
+
+ //Constructor 
+ public LibraryItem(String id, String title, String author, String isbn, String publisher, String copies) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.publisher = publisher;
+ }
+
 
  
 }
