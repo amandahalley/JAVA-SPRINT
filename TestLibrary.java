9313f6fb47b5a6439223e4a4c5691b35bfdc23ca
ahllyy <amanda.halley@keyin.com>
2024-10-23 19:54:39 -0230

    created for unit testing on classes

---
+++ TestLibrary.java
@@ -0,0 +1,14 @@
+public class TestLibrary {
+    public static void main(String[] args) {
+        
+    
+        //Testing for constructor and toString method 
+        LibraryItem item = new LibraryItem("1", "Book Title", "Amanda Halley", "9780134685991", "John Doe", 3);
+
+        System.out.println();
+        System.out.println("Library Item Details:");
+        System.out.println(item);
+    };
+};
+
+
