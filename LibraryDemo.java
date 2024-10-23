c8885555d0b0986d02a791f1a7a71d1f827dd840
ahllyy <amanda.halley@keyin.com>
2024-10-23 19:55:43 -0230

    main entry point for application

--- LibraryDemo.java
+++ LibraryDemo.java
@@ -1,13 +1,5 @@
 public class LibraryDemo {
     public static void main(String[] args) {
 
-        //Testing for constructor and toString method 
-        LibraryItem item = new LibraryItem("1", "Book Title", "Amanda Halley", "9780134685991", "John Doe", 3);
-
-        System.out.println();
-        System.out.println("Library Item Details:");
-        System.out.println(item);
-    };
-
    
 }
