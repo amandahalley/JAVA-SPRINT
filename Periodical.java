95584423fd3251a73e37a3fabdcf968ea1c1d00f
ahllyy <amanda.halley@keyin.com>
2024-10-25 16:12:26 -0230

    setting up the constructor and format

--- Periodical.java
+++ Periodical.java
@@ -1,3 +1,18 @@
 public class Periodical extends LibraryItem {
-    
+    private String Printed = "Printed";
+    private String Electronic = "Electronic";
+    private String format;
+
+    //Constructor 
+   public Periodical(String id, String title, String author, String isbn, String publisher, int copies, String format) {
+    //Calling libraryItem constructor (superclass) and setting format
+    super(id, title, author, isbn, publisher, copies);
+    setFormat(format);
+
+   }
+
+   public String getFormat() {
+    return format();
+   }
+
 }
