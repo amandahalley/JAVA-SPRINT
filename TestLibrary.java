01f63543b0d0d74441b737d8d24b0e9264617ac0
ahllyy <amanda.halley@keyin.com>
2024-10-25 15:47:41 -0230

    added testing for book class

--- TestLibrary.java
+++ TestLibrary.java
@@ -8,7 +8,24 @@ public class TestLibrary {
         System.out.println();
         System.out.println("Library Item Details:");
         System.out.println(item);
+    
+
+
+
+    //Testing valid book creating for book class
+        try {
+            Book valid = new Book("2", "Book 2", "Jane Smith", "9780134123948", "John Doe", 2, "Audio");
+            System.out.println();
+            System.out.println("Valid book created");
+            System.out.println(valid);
+        } catch (IllegalArgumentException e)  {
+            System.out.println("Test failed" + e.getMessage());
+        }
+
     };
+
 };
 
 
+
+    
