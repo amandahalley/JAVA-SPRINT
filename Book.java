bb79bb40877b68ddec144ba2be4611964eb73438
ahllyy <amanda.halley@keyin.com>
2024-10-25 15:04:48 -0230

    adding format attributes for book class (printed, audio, electronic)

--- Book.java
+++ Book.java
@@ -1,3 +1,7 @@
 public class Book extends LibraryItem {
+    private String Printed= "Printed";
+    private String Audio = "Audio";
+    private String Electronic = "Electronic";
 
+    private String format;
 }
