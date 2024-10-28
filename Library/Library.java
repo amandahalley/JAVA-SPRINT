e48ca59326744e28a613edacb6121f3e66fc8151
ahllyy <amanda.halley@keyin.com>
2024-10-28 16:08:05 -0230

    created lists for LibraryItems, Authors and Patrons. Methods to be implemented

--- Library/Library.java
+++ Library/Library.java
@@ -1,8 +1,20 @@
 package Library;
+// imports to be added
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
+    //Creating lists specifying what objects each list can hold 
+    private List <LibraryItem> items = new ArrayList<>(); //List of LibraryItem (Books and Periodicals)
+    private List <Author> authors = new ArrayList<>(); //List of Authors 
+    private List <Patron> patrons = new ArrayList<>(); //List of Patrons (Students or Employees)
 
-}
+
+    public Library() { // empty consturctor as lists have already been initialized 
+    };
+
+
+    // Methods to be added..
+
+};
