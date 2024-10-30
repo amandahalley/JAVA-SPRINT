de53ed880a33a4d1e70b21a7c6f6d51c1ce960a2
ahllyy <amanda.halley@keyin.com>
2024-10-30 11:38:06 -0230

    added methods for item managedment (add, edit and delete) - commented out patron imports for testing since file is not created yet

--- Library/Library.java
+++ Library/Library.java
@@ -1,6 +1,6 @@
 package Library;
 import People.Author;
-import People.Patron;
+// import People.Patron;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,13 +9,56 @@ public class Library {
     //Creating lists specifying what objects each list can hold 
     private List <LibraryItem> items = new ArrayList<>(); //List of LibraryItem (Books and Periodicals)
     private List <Author> authors = new ArrayList<>(); //List of Authors 
-    private List <Patron> patrons = new ArrayList<>(); //List of Patrons (Students or Employees)
+    // private List <Patron> patrons = new ArrayList<>(); //List of Patrons (Students or Employees)
 
 
     public Library() { // empty consturctor as lists have already been initialized 
     };
 
 
-    // Methods to be added..
+    //Item management methods
+    //Add item - adds item to the collection, if left empty error message occurs.
+    public void addItem(LibraryItem item) {
+        if (item != null) {
+            items.add(item);
+        System.out.println("Item succesfully added: " + item.getTitle());        
+    } else {
+        System.out.println("Can not add item. Please try again.");
+        }
+    }
+
+    //Edit item - updates item existing in collection by matching itemID and updating items attributes
+    public boolean editItem(String itemID, LibraryItem updatedItem) {
+        for (LibraryItem item : items) {  //cycles through items and finds matching id then updates properties with new details
+            if (item.getID().equals(itemID)) { 
+                item.setTitle(updatedItem.getTitle());
+                item.setAuthor(updatedItem.getAuthor());
+                item.setISBN(updatedItem.getISBN());
+                item.setPublisher(updatedItem.getPublisher());
+                item.setCopies(updatedItem.getCopies());
+
+                System.out.println("Item successfully updated: " + updatedItem.getTitle());
+                return true;
+
+            }
+        }
+        //print statement if itemid is not found
+        System.out.println("Item ID: " + itemID + " not found.");
+        return false;
+    }
+
+    //Delete item from collection
+    public boolean deleteItem(String itemID) {
+        for (LibraryItem item : items) { //cycles through items finding matching id, if id matches item is removed
+            if (item.getID().equals(item)) {
+                items.remove(item);
+                System.out.println("Item successfully deleted: " + item.getTitle());
+                return true;
+            }
+        }
+        //print statement if itemid is not found
+        System.out.println("Item ID: " + itemID + "not found.");
+        return false; 
+    }
 
 };
