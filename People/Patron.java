4c13dbe3c6046ef7c8ead2662449f71390cd7a17
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2024-10-30 13:42:47 -0230

    Patron strings added

--- People/Patron.java
+++ People/Patron.java
@@ -1,1 +1,23 @@
 package People;
+
+import Library.LibraryItem;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patron {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private List<LibraryItem> borrowedItems;
+
+public Patron(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.borrowedItems = new ArrayList<>();
+    }
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public List<LibraryItem> getBorrowedItems() { return borrowedItems; }
+}
